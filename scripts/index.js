57bc2edde9b67173b4f3dbecbf776503da5de168
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-31 16:48:06 -0400

    Updated index

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,4 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
+/* Homepage JavaScript file */
 
 document.addEventListener('DOMContentLoaded', () => {
   const login_button = document.getElementById("login-button");
@@ -9,7 +6,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (authToken) {
     login_button.style.display = "none";
+    fetchPlaces(authToken);
   } else {
     login_button.style.display = "block";
-  } 
+  };
+
+  async function fetchPlaces(token) {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/places', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+
+      if (response.ok) {
+        const places = await response.json();
+        displayPlaces(places);
+        // populateCountryFilter(places);
+      } else {
+        console.error('Failed to load places:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  function displayPlaces(places) {
+    const placesList = document.getElementById('places-list');
+    placesList.innerHTML = '';
+
+    places.forEach(place => {
+      const placeElement = document.createElement('div');
+      placeElement.classList.add('place');
+      placeElement.dataset.country = place.country;
+      placeElement.innerHTML =
+        `<h2>${place.name}</h2>
+         <p>${place.description}</p>
+         <p><strong>Location:</strong> ${place.location}</p>`;
+      placesList.appendChild(placeElement);
+    });
+  }
 });
